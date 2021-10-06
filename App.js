70f895b06268d306f5edfa5916cfdcba2f8f957e
Sarah Fluck <SarahCoded@users.noreply.github.com>
2021-10-06 14:38:11 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,114 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ const Section = ({children, title}): Node => {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}>
+         {children}
+       </Text>
+     </View>
+   );
+ };
+ 
+ const App: () => Node = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+ 
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+   const date = new Date("2012-05-24");
+    const number = 43228.32;
+    const local = navigator.language;
+    const foodList = ['Noodles', 'Chicken', 'Pasta'];
+   return (
+    
+     <SafeAreaView style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <ScrollView
+         contentInsetAdjustmentBehavior="automatic"
+         style={backgroundStyle}>
+         <Header />
+         <View
+           style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+           }}>
+           <Section title="Intl Testing">
+              <Text>Date: {new Intl.DateTimeFormat(local).format(date)}</Text>
+              {"\n"}
+              <Text>Currency: {new Intl.NumberFormat(local, { style: 'currency', currency: 'GBP' }).format(number)}</Text>
+              {"\n"}
+              <Text>Number Format: {new Intl.NumberFormat(local).format(1234567890)}</Text>
+              {"\n"}
+              <Text>Relative Time: {new Intl.RelativeTimeFormat(local, { numeric: "auto" }).format(10, 'seconds')}</Text>
+              {"\n"}
+              <Text>List Format: {new Intl.ListFormat(local).format(foodList)}</Text>
+              {"\n"}
+           </Section>
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
+ 
+ export default App;
